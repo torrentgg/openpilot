c5f0b073a521b7c3b7edba25aba3345660d108dc
Fraser Xiong <xfraser@student.ubc.ca>
2022-08-23 05:24:43 -0700

    Delete all

--- force_car_recognition.py
+++
@@ -1,8 +0,0 @@
-filenames = ['/data/params/d/HondaCars', '/data/params/d/HyundaiCars', '/data/params/d/SubaruCars',
-             '/data/params/d/ToyotaCars', '/data/params/d/VolkswagenCars']
-
-with open('/data/params/d/Cars', 'w') as outfile:
-  for names in filenames:
-    with open(names) as infile:
-      outfile.write(infile.read())
-    outfile.write("\n")
