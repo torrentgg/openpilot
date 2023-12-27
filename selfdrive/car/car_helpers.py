28e1eea9e895449ed1b5a71e4facbbe48ba29ba3
torrentgg <154601738+torrentgg@users.noreply.github.com>
2023-12-27 12:53:42 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -180,7 +180,7 @@ def get_car(logcan, sendcan):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "HONDA PILOT 2017"
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, car_fw)
